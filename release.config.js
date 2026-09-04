8bcf78ad0c6f8b2afa0d48e03a952fcef804b247
Niklas Beilmann <niklas.beilmann@telekom.de>
2026-09-04 15:41:46 +0000

    ci: qualify images before publishing

--- release.config.js
+++ release.config.js
@@ -1,46 +1,35 @@
 module.exports = {
-    branches: ['main'],
-    repositoryUrl: 'git@github.com:telekom/gateway-issuer-service-go.git',
-    tagFormat: '${version}',
-    plugins: [
-        [
-          '@semantic-release/commit-analyzer',
-          {
-            releaseRules: [
-              { type: 'build', release: 'patch' },
-              { type: 'chore', release: 'patch' },
-              { type: 'ci', release: 'patch' },
-              { type: 'docs', release: 'patch' },
-              { type: 'perf', release: 'patch' },
-              { type: 'refactor', release: 'patch' },
-              { type: 'revert', release: 'patch' },
-              { type: 'style', release: 'patch' },
-              { type: 'test', release: 'patch' },
-            ]
-          }
+  branches: [
+    { name: 'main', channel: false },
+    { name: 'next', prerelease: 'rc', channel: 'next' },
+  ],
+  repositoryUrl: 'git@github.com:telekom/gateway-issuer-service-go.git',
+  tagFormat: '${version}',
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'conventionalcommits',
+        releaseRules: [
+          { breaking: true, release: 'major' },
+          { revert: true, release: false },
         ],
-        'semantic-release-export-data',
-        [
-          '@semantic-release/release-notes-generator',
-          {
-            preset: 'conventionalcommits',
-            presetConfig: {
-              types: [
-                { type: 'feat', section: 'Features', hidden: false },
-                { type: 'fix', section: 'Bug Fixes', hidden: false },
-                { type: 'build', section: 'Build System', hidden: false },
-                { type: 'chore', section: 'Chores', hidden: false },
-                { type: 'ci', section: 'Continuous Integration', hidden: false },
-                { type: 'docs', section: 'Documentation', hidden: false },
-                { type: 'perf', section: 'Performance Improvements', hidden: false },
-                { type: 'refactor', section: 'Code Refactoring', hidden: false },
-                { type: 'revert', section: 'Reverts', hidden: false },
-                { type: 'style', section: 'Styles', hidden: false },
-                { type: 'test', section: 'Tests', hidden: false },
-              ]
-            }
-          }
-        ],
-        '@semantic-release/github',
+      },
     ],
+    'semantic-release-export-data',
+    [
+      '@semantic-release/release-notes-generator',
+      {
+        preset: 'conventionalcommits',
+        presetConfig: {
+          types: [
+            { type: 'feat', section: 'Features', hidden: false },
+            { type: 'fix', section: 'Bug Fixes', hidden: false },
+            { type: 'perf', section: 'Performance Improvements', hidden: false },
+          ],
+        },
+      },
+    ],
+    '@semantic-release/github',
+  ],
 };
